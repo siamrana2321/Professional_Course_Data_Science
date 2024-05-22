55933f5eb11dd6bcddcab42d9dd43d4865ab38a1
siamrana2321 <msar572321@gmail.com>
2024-05-23 01:58:55 +0600

    2nd Commit

--- Python_Learning/17.py
+++ Python_Learning/17.py
@@ -3,6 +3,10 @@ num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
 num3 = float(input("Enter third number: "))
 
-max_num = max(num1, num2, num3)
+if num1 >= num2 and num1 >= num3:
+    print("Maximum number:", num1)
+elif num2 >= num1 and num2 >= num3:
+    print("Maximum number:", num2)
+else :
+    print("Maximum number:", num3)
 
-print("Maximum number:", max_num)
